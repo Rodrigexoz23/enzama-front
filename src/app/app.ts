66945b91b624ac35117065c74b61cfb6aa57d4b4
Rodrigo Ramos <ramosarreolarodrigo@gmail.com>
2026-01-13 22:23:36 -0600

    [RRA] Se arregla navbar para que solo se muestre si el usuario esta logeado

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,13 +3,20 @@ import { RouterOutlet } from '@angular/router';
 import { Clientas } from './clientas/clientas';
 import { Navbar } from "./navbar/navbar";
 import { Footer } from './footer/footer';
+import { Auth } from './services/auth';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, Footer],
+  imports: [RouterOutlet, Navbar, Footer, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+  constructor (public auth: Auth){}
   protected readonly title = signal('enzama-app');
 }
+
+
+
